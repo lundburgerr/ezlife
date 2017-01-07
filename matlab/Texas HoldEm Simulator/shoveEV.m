70c69839567b1bbb7091618e86b11e0bb71c2adb
lundburgerr <lundburgerr@gmail.com>
2017-01-07 11:30:06 +0100

    A working shoveEV for both direct calculation of bestHand every loop (commented) but also mainly for indexed handvalues

--- matlab/Texas HoldEm Simulator/shoveEV.m
+++ matlab/Texas HoldEm Simulator/shoveEV.m
@@ -1,22 +1,50 @@
 
+S = load('allHandRankings.mat', 'allHandRankings');
+allHandRankings = S.allHandRankings;
 
-p1 = mapCardsText2Num('8D, 7D', 2);
-p2 = mapCardsText2Num('AS, JS', 2);
+%Necessary for fast nCk
+nCk_mapping = zeros(51, 6);
+for k=1:7
+    for n=k:51
+        nCk_mapping(n,k) = nchoosek(n, k-1);
+    end
+end
+
+p1 = mapCardsText2Num('8C, 7D', 2);
+p2 = mapCardsText2Num('AS, AH', 2);
 Deck = 1:52;
-Deck(p1) = [];
-Deck(p2) = [];
+idx = ismember(Deck, p1); Deck(idx) = [];
+idx = ismember(Deck, p2); Deck(idx) = [];
 
-numSim = 30000;
+numSim = 100000;
 p1wins = 0;
 p2wins = 0;
 chop = 0;
 tic
-parfor n=1:numSim
-   table = DrawCards(5, Deck);
-   [ranking, result] = HandResult([p1; p2], table);
-   p1wins = p1wins + (ranking(1)<ranking(2));
-   p2wins = p2wins + (ranking(1)>ranking(2));
-   chop = chop + (ranking(1)==ranking(2));
+for n=1:numSim
+    % Draw 5 cards
+   table = drawCardsFast(5, Deck);
+   
+   %Calculate handRanking directly
+%    ranking = handComparison([p1; p2], table);
+%    r1 = ranking(1);
+%    r2 = ranking(2);
+
+    %Calculate handRanking fast from precalculated array
+    idx1 = handIndex([p1, table], 52, 7, nCk_mapping);
+    idx2 = handIndex([p2, table], 52, 7, nCk_mapping);
+    r1 = allHandRankings(idx1);
+    r2 = allHandRankings(idx2);
+    
+    %Compare ranking between hands
+    if r1 < r2
+        p1wins = p1wins + 1;
+    elseif r1 > r2
+        p2wins = p2wins + 1;
+    else
+        chop = chop + 1;
+    end
+    
 end
 toc
 
