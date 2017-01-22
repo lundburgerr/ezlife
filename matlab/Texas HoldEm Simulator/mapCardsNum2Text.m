6630a03dbe318072c8793be465b7b96a3d6722a4
lundburgerr <lundburgerr@gmail.com>
2017-01-22 15:34:54 +0100

    Create string describing an array of cards from numeric values

---
+++ matlab/Texas HoldEm Simulator/mapCardsNum2Text.m
@@ -0,0 +1,20 @@
+function cardsText = mapCardsNum2Text(cardsNum)
+Cards = {'A','K','Q','J','10','9','8','7','6','5','4','3','2'};
+Suits = {'S','D','H','C'};
+
+if isempty(cardsNum)
+    cardsText = blanks(0);
+    return;
+end
+
+inputCards = ceil(cardsNum./4);
+inputSuits = (cardsNum-1) - 4.*floor((cardsNum-1)./4) + 1; %mod(cardsNum-1, 4) + 1; %Fast modulus mod(x,a) = x - a.*floor(x/4)
+N = length(cardsNum);
+cardsText = blanks(2*N + 2*(N-1));
+cardsText(1) = Cards{inputCards(1)};
+cardsText(2) = Suits{inputSuits(1)};
+for n=2:N
+    cardsText(4*(n-1) - 1) = ',';
+    cardsText(4*(n-1) + 1) = Cards{inputCards(n)};
+    cardsText(4*(n-1) + 2) = Suits{inputSuits(n)};
+end
