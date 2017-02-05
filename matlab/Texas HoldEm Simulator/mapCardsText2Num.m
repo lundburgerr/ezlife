00807452e7ec1b5129ee7d8e73a3714ecc394803
lundburgerr <lundburgerr@gmail.com>
2017-02-05 12:31:31 +0100

    More general and better solution of converting card strings to card number

--- matlab/Texas HoldEm Simulator/mapCardsText2Num.m
+++ matlab/Texas HoldEm Simulator/mapCardsText2Num.m
@@ -1,15 +1,14 @@
-function cards = mapCardsText2Num(cardStr,num)
+function cards = mapCardsText2Num(cardStr)
 %Given a card in string return the mapping in an integer from 1 to 52
 %cardStr    - format is like 'AS JD' with valid cards from A, K, Q, J, 10, 9,
 %               ... and valid suits are S, D, H, C. Cards are seperated by
 %               comma or space
 %num        - Number of cards to map from cardStr, num cannot be more than
 %               the number of cards in cardStr
-    cards = zeros(1,num);
-    for idx = 1:num
-        [c cardStr] = strtok(cardStr,[',',' ']); %#ok
-        cardStr = cardStr(2:end);
-        cards(idx) = ParseUserCard(c);
+    cardsText = regexp(cardStr, '[AKQJTakqjt98765432]{1,1}[SDCHsdch]{1,1}', 'match');
+    cards = zeros(length(cardsText),1);
+    for idx = 1:length(cardsText)
+        cards(idx) = ParseUserCard(cardsText{idx});
     end
 end
 
